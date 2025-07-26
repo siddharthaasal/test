6a785654f6977e89382459ee357ae1d5f1f81f29
siddharthaasal <siddharthaasal@gmail.com>
2025-07-26 13:42:44 +0530

    footer, policies

--- src/pages/LandingPage2.tsx
+++ src/pages/LandingPage2.tsx
@@ -1,11 +1,13 @@
 import Navbar2 from "../components/Navbar2";
 import HeroSection2 from "../sections/HeroSection2";
+import { Footer2 } from "../components/Footer";
 
 export default function LandingPage() {
     return (
         <>
             <Navbar2 />
             <HeroSection2 />
+            <Footer2 />
         </>
     );
 }
